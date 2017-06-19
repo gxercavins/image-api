007442ac094b3f77734881259952345c6bfda331
gxercavins <guillemxercavins@gmail.com>
2017-06-19 13:50:58 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,174 @@
+from flask import Flask, request, render_template, send_from_directory
+import os
+from PIL import Image
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+# default access page
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+
+# upload selected image and forward to processing page
+@app.route("/upload", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'static/images/')
+
+    # create image directory if not found
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    # retrieve file from html file-picker
+    upload = request.files.getlist("file")[0]
+    print("File name: {}".format(upload.filename))
+    filename = upload.filename
+
+    # file support verification
+    ext = os.path.splitext(filename)[1]
+    if (ext == ".jpg") or (ext == ".png") or (ext == ".bmp"):
+        print("File accepted")
+    else:
+        return render_template("error.html", message="The selected file is not supported")
+
+    # save file
+    destination = "/".join([target, filename])
+    print("File saved to to:", destination)
+    upload.save(destination)
+
+    # forward to processing page
+    return render_template("processing.html", image_name=filename)
+
+
+# rotate filename the specified degrees
+@app.route("/rotate", methods=["POST"])
+def rotate():
+    # retrieve parameters from html form
+    angle = request.form['angle']
+    filename = request.form['image']
+
+    # open and process image
+    target = os.path.join(APP_ROOT, 'static/images')
+    destination = "/".join([target, filename])
+
+    img = Image.open(destination)
+    img = img.rotate(-1*int(angle))
+
+    # show modified image
+    img.show()
+    return '', 204
+
+
+# flip filename 'vertical' or 'horizontal'
+@app.route("/flip", methods=["POST"])
+def flip():
+
+    # retrieve parameters from html form
+    if 'horizontal' in request.form['mode']:
+        mode = 'horizontal'
+    else:
+        mode = 'vertical'
+    filename = request.form['image']
+
+    # open and process image
+    target = os.path.join(APP_ROOT, 'static/images')
+    destination = "/".join([target, filename])
+
+    img = Image.open(destination)
+
+    if mode == 'horizontal':
+        img = img.transpose(Image.FLIP_LEFT_RIGHT)
+    else:
+        img = img.transpose(Image.FLIP_TOP_BOTTOM)
+
+    # show modified image
+    img.show()
+    return '', 204
+
+
+# crop filename from (x1,y1) to (x2,y2)
+@app.route("/crop", methods=["POST"])
+def crop():
+    # retrieve parameters from html form
+    x1 = int(request.form['x1'])
+    y1 = int(request.form['y1'])
+    x2 = int(request.form['x2'])
+    y2 = int(request.form['y2'])
+    filename = request.form['image']
+
+    # open image
+    target = os.path.join(APP_ROOT, 'static/images')
+    destination = "/".join([target, filename])
+
+    img = Image.open(destination)
+
+    # check for valid crop parameters
+    width = img.size[0]
+    height = img.size[1]
+
+    crop_possible = True
+    if not 0 <= x1 < width:
+        crop_possible = False
+    if not 0 < x2 <= width:
+        crop_possible = False
+    if not 0 <= y1 < height:
+        crop_possible = False
+    if not 0 < y2 <= height:
+        crop_possible = False
+    if not x1 < x2:
+        crop_possible = False
+    if not y1 < y2:
+        crop_possible = False
+
+    # crop image and show
+    if crop_possible:
+        img.crop((x1, y1, x2, y2)).show()
+    else:
+        return render_template("error.html", message="Crop dimensions not valid")
+    return '', 204
+
+
+# blend filename with stock photo and alpha parameter
+@app.route("/blend", methods=["POST"])
+def blend():
+    # retrieve parameters from html form
+    alpha = request.form['alpha']
+    filename1 = request.form['image']
+
+    # open images
+    target = os.path.join(APP_ROOT, 'static/images')
+    filename2 = 'blend.jpg'
+    destination1 = "/".join([target, filename1])
+    destination2 = "/".join([target, filename2])
+
+    img1 = Image.open(destination1)
+    img2 = Image.open(destination2)
+
+    # resize images to max dimensions
+    width = max(img1.size[0], img2.size[0])
+    height = max(img1.size[1], img2.size[1])
+
+    img1 = img1.resize((width, height), Image.ANTIALIAS)
+    img2 = img2.resize((width, height), Image.ANTIALIAS)
+
+    # if image in gray scale, convert stock image to monochrome
+    if len(img1.mode) < 3:
+        img2 = img2.convert('L')
+
+    # blend and show image
+    Image.blend(img1, img2, float(alpha)/100).show()
+
+    return '', 204
+
+
+# retrieve file from 'static/images' directory
+@app.route('/static/images/<filename>')
+def send_image(filename):
+    return send_from_directory("static/images", filename)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ core.py
@@ -0,0 +1,164 @@
+from flask import Flask, request, render_template, send_from_directory
+import os
+from PIL import Image
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+# default access redirects to API documentation
+@app.route("/")
+def main():
+    return '', 204
+
+
+# rotate filename the specified degrees
+@app.route("/rotate/<angle>/<filename>", methods=["GET"])
+def rotate(angle, filename):
+
+    # check for valid angle
+    angle = int(angle)
+    if not -360 < angle < 360:
+        return render_template("error.html", message="Invalid angle parameter (-359 to 359)")
+
+    # open and process image
+    target = os.path.join(APP_ROOT, 'static/images')
+    destination = "/".join([target, filename])
+
+    img = Image.open(destination)
+    img = img.rotate(-1*angle)
+
+    # save and return image
+    destination = "/".join([target, 'temp.png'])
+    if os.path.isfile(destination):
+        os.remove(destination)
+    img.save(destination)
+
+    return send_image('temp.png')
+
+
+# flip filename 'vertical' or 'horizontal'
+@app.route("/flip/<mode>/<filename>", methods=["GET"])
+def flip(mode, filename):
+
+    # open and process image
+    target = os.path.join(APP_ROOT, 'static/images')
+    destination = "/".join([target, filename])
+
+    img = Image.open(destination)
+
+    # check mode
+    if mode == 'horizontal':
+        img = img.transpose(Image.FLIP_LEFT_RIGHT)
+    elif mode == 'vertical':
+        img = img.transpose(Image.FLIP_TOP_BOTTOM)
+    else:
+        return render_template("error.html", message="Invalid mode (vertical or horizontal)")
+
+    # save and return image
+    destination = "/".join([target, 'temp.png'])
+    if os.path.isfile(destination):
+        os.remove(destination)
+    img.save(destination)
+
+    return send_image('temp.png')
+
+
+# crop filename from (x1,y1) to (x2,y2)
+@app.route("/crop/<x1>/<y1>/<x2>/<y2>/<filename>", methods=["GET"])
+def crop(x1, y1, x2, y2, filename):
+
+    # open image
+    target = os.path.join(APP_ROOT, 'static/images')
+    destination = "/".join([target, filename])
+
+    img = Image.open(destination)
+    width = img.size[0]
+    height = img.size[1]
+
+    # check for valid crop parameters
+    x1 = int(x1)
+    y1 = int(y1)
+    x2 = int(x2)
+    y2 = int(y2)
+
+    crop_possible = True
+    if not 0 <= x1 < width:
+        crop_possible = False
+    if not 0 < x2 <= width:
+        crop_possible = False
+    if not 0 <= y1 < height:
+        crop_possible = False
+    if not 0 < y2 <= height:
+        crop_possible = False
+    if not x1 < x2:
+        crop_possible = False
+    if not y1 < y2:
+        crop_possible = False
+
+    # process image
+    if crop_possible:
+        img = img.crop((x1, y1, x2, y2))
+    else:
+        return render_template("error.html", message="Crop dimensions not valid")
+
+    # save and return image
+    destination = "/".join([target, 'temp.png'])
+    if os.path.isfile(destination):
+        os.remove(destination)
+    img.save(destination)
+
+    return send_image('temp.png')
+
+
+# blend filename1 and filename2 with alpha parameter
+@app.route("/blend/<alpha>/<filename1>/<filename2>", methods=["GET"])
+def blend(alpha, filename1, filename2):
+
+    # check for valid alpha
+    alpha = float(alpha)
+    if not 0 <= alpha <= 100:
+        return render_template("error.html", message="Invalid alpha value (0-100)")
+
+    #open images
+    target = os.path.join(APP_ROOT, 'static/images')
+    destination1 = "/".join([target, filename1])
+    destination2 = "/".join([target, filename2])
+
+    img1 = Image.open(destination1)
+    img2 = Image.open(destination2)
+
+    # check for dimensions and resize to larger ones
+    width = max(img1.size[0], img2.size[0])
+    height = max(img1.size[1], img2.size[1])
+
+    img1 = img1.resize((width, height), Image.ANTIALIAS)
+    img2 = img2.resize((width, height), Image.ANTIALIAS)
+
+    # if one image in gray scale, convert the other to monochrome
+    if len(img1.mode) < 3:
+        img2 = img2.convert('L')
+    elif len(img2.mode) < 3:
+        img1 = img1.convert('L')
+
+    # blend images
+    img = Image.blend(img1, img2, float(alpha)/100)
+
+    # save and return
+    destination = "/".join([target, 'temp.png'])
+    if os.path.isfile(destination):
+        os.remove(destination)
+    img.save(destination)
+
+    return send_image('temp.png')
+
+
+# retrieve file from 'static/images' directory
+@app.route('/static/images/<filename>')
+def send_image(filename):
+    return send_from_directory("static/images", filename)
+
+
+if __name__ == "__main__":
+    app.run()
